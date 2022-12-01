3033f4e2a474c58251e2f0784480f954f18483c6
VictorRotha <Victor.Rotha@t-online.de>
2022-12-01 10:37:22 +0100

    day01 part 1

--- src/day01/Day01.java
+++ src/day01/Day01.java
@@ -1,9 +1,42 @@
 package day01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Day01 {
 
 
     public static void main(String[] args) {
-        System.out.println("Day01");
+
+
+        int calories = 0;
+        int maxCalories = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/day01/input.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+
+                    maxCalories = Math.max(maxCalories, calories);
+                    calories = 0;
+
+                } else {
+                    calories += Integer.parseInt(line);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        maxCalories = Math.max(maxCalories, calories);
+
+        System.out.println("MaxCalories: " + maxCalories);
+
     }
+
+
+
 }
