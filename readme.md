4bfd419c2df4d171b8506bfc34d58cc83ea0452b
VictorRotha <Victor.Rotha@t-online.de>
2022-12-01 12:05:39 +0100

    day 02 part 2

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Advent of Code 2022
 
-Java
+[Advent of Code 2022](https://adventofcode.com/2022)
+
+- [Day 01](src/day01/Day01.java)
--- src/day01/Day01.java
+++ src/day01/Day01.java
@@ -3,40 +3,46 @@ package day01;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day01 {
 
 
     public static void main(String[] args) {
 
-
         int calories = 0;
-        int maxCalories = 0;
+        ArrayList<Integer> elves = new ArrayList<>();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("src/day01/input.txt"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.isEmpty()) {
+            String line ;
 
-                    maxCalories = Math.max(maxCalories, calories);
+            do {
+                line = br.readLine();
+                if (line == null || line.isEmpty()) {
+                    elves.add(calories);
                     calories = 0;
-
                 } else {
                     calories += Integer.parseInt(line);
                 }
 
-            }
+            } while (line != null);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        maxCalories = Math.max(maxCalories, calories);
+        Collections.sort(elves);
+        int s = elves.size();
+        int topSum = elves.get(s-1) + elves.get(s-2) + elves.get(s-3);
+        int maxCalories = elves.get(s-1);
 
-        System.out.println("MaxCalories: " + maxCalories);
+        System.out.println("MaxCalories:    " + maxCalories);
+        System.out.println("Top 3 Calories: " + topSum);
 
     }
-
 
 
 }
