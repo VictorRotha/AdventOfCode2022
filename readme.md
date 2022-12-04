f5abca049c0405462e129ffc72ededc0883ccaaa
VictorRotha <Victor.Rotha@t-online.de>
2022-12-04 10:32:58 +0100

    day 04 part 2

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@
 - [Day 01: Calorie Counting](src/day01/Day01.java)
 - [Day 02: Rock Paper Scissors](src/day02/Day02.java)
 - [Day 03: Rucksack Reorganization](src/day03/Day03.java)
+- [Day 04: Camp Cleanup](src/day04/Day04.java)
--- src/day04/Day04.java
+++ src/day04/Day04.java
@@ -12,19 +12,23 @@ public class Day04 {
 
         try (BufferedReader br = new BufferedReader(new FileReader("src/day04/input.txt")))  {
 
-            int sum = 0;
+            int includedSum = 0;
+            int overlapSum = 0;
             String line;
             while ((line = br.readLine()) != null) {
 
                 int[] i = Arrays.stream(line.split("[-,]")).mapToInt(Integer::parseInt).toArray();
 
                 if (isIncluded(i))
-                    sum++;
+                    includedSum++;
+
+                if (isOverlapping(i))
+                    overlapSum++;
 
             }
 
-            System.out.println("Part 01 result: " + sum);
-
+            System.out.println("Part 01 result: " + includedSum);
+            System.out.println("Part 02 result: " + overlapSum);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,6 +40,12 @@ public class Day04 {
     public static boolean isIncluded(int[] i) {
 
         return (i[0] >= i[2] && i[1] <= i[3]) || (i[2] >= i[0] && i[3] <= i[1]);
+    }
+
+    public static boolean isOverlapping(int[] i) {
+
+        return (i[0] <= i[2] && i[2] <= i[1]) || (i[2] <= i[0] && i[0] <= i[3]);
+
     }
 
 
